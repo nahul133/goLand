d35e06077d4e72e31eff38e204b8071975d499d9
Nahuel Doval <dovalnahuel@gmail.com>
2023-10-03 14:44:15 -0300

    segundo commit

--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/nahul133/goLand/funciones"
 )
 
 func main() {
-	estado, texto := funciones.ConvertiraTexto(10)
+	fmt.Println("*EXECUTE*")
 
-	fmt.Println(estado)
-	fmt.Println(texto)
 }
